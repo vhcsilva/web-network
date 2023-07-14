70da789c1c02d4f5b3a8fb71d46bc0adcbcf310a
Vitor Hugo <vhcsilvaa@gmail.com>
2023-07-14 09:36:47 -0300

    make management tab use server side data

--- components/profile/my-network-settings/management.tsx
+++ components/profile/my-network-settings/management.tsx
@@ -1,7 +1,13 @@
 import BountiesList from "components/bounty/bounties-list/controller";
 
+import { SearchBountiesPaginated } from "types/api";
+
+interface ManagementProps {
+  bounties: SearchBountiesPaginated;
+}
+
 export default function Management({
   bounties
-}) {
+}: ManagementProps) {
   return <BountiesList bounties={bounties} variant="management" />;
 }
